2149cad74c0d3df878bdfd9b0a473266b10f2015
Zpsch <162704483+Zpsch@users.noreply.github.com>
2024-04-14 21:11:35 -0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const keep_alive = require('./keep_alive.js');
 const { Client, IntentsBitField, InteractionCollector } = require('discord.js');
 
 const client = new Client({
